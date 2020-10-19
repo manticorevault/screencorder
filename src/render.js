8c82c3434b3030af2d69ac6025b9b3dd33ff14a0
manticorevault <artur_seilah@hotmail.com>
2020-10-19 13:39:23 +0100

    Select Window and Preview Video Stream working

---
+++ src/render.js
@@ -0,0 +1,49 @@
+const videoElement = document.querySelector("video")
+const startBtn = document.getElementById("startBtn")
+const stopBtn = document.getElementById("stopBtn")
+const videoSelectBtn = document.getElementById("videoSelectBtn")
+videoSelectBtn.onclick = getVideoSources
+
+const { writeFile } = require("fs");
+
+const { desktopCapturer, remote } = require("electron")
+const { Menu } = remote;
+
+async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+        types: ["window", "screen"]
+    })
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            }
+        })
+    )
+
+    videoOptionsMenu.popup();
+}
+
+
+async function selectSource(source) {
+
+    videoSelectBtn.innerText = source.name;
+
+    const constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: "desktop",
+                chromeMediaSourceId: source.id
+            }
+        }
+    }
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+    videoElement.srcObject = stream;
+    videoElement.play();
+}
+
