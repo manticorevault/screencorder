9d6ca2ffe02d64948011c9ddc57c5d30e09c221b
manticorevault <artur_seilah@hotmail.com>
2020-10-19 14:19:09 +0100

    Fixed the bugs. App working properly

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
+      nodeIntegration: true
     }
   });
 
--- src/render.js
+++ src/render.js
@@ -1,32 +1,55 @@
-const videoElement = document.querySelector("video")
-const startBtn = document.getElementById("startBtn")
-const stopBtn = document.getElementById("stopBtn")
-const videoSelectBtn = document.getElementById("videoSelectBtn")
-videoSelectBtn.onclick = getVideoSources
+const { desktopCapturer, remote } = require('electron');
+const { writeFile } = require('fs');
+const { dialog, Menu } = remote;
 
-const { writeFile } = require("fs");
+// Global state
+let mediaRecorder;
+const recordedChunks = [];
 
-const { desktopCapturer, remote } = require("electron")
-const { Menu } = remote;
+// Defining the buttons
+const videoElement = document.querySelector('video');
 
+const startBtn = document.getElementById('startBtn');
+
+startBtn.onclick = e => {
+
+    mediaRecorder.start();
+    startBtn.classList.add('is-danger');
+    startBtn.innerText = 'Recording';
+};
+
+const stopBtn = document.getElementById('stopBtn');
+
+stopBtn.onclick = e => {
+
+    mediaRecorder.stop();
+    startBtn.classList.remove('is-danger');
+    startBtn.innerText = 'Start';
+};
+
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+videoSelectBtn.onclick = getVideoSources;
+
+// Fetch the video sources from a pop-up window
 async function getVideoSources() {
     const inputSources = await desktopCapturer.getSources({
-        types: ["window", "screen"]
-    })
+        types: ['window', 'screen']
+    });
 
     const videoOptionsMenu = Menu.buildFromTemplate(
         inputSources.map(source => {
             return {
                 label: source.name,
                 click: () => selectSource(source)
-            }
+            };
         })
-    )
+    );
+
 
     videoOptionsMenu.popup();
 }
 
-
+// Selects the video source
 async function selectSource(source) {
 
     videoSelectBtn.innerText = source.name;
@@ -35,15 +58,51 @@ async function selectSource(source) {
         audio: false,
         video: {
             mandatory: {
-                chromeMediaSource: "desktop",
+                chromeMediaSource: 'desktop',
                 chromeMediaSourceId: source.id
             }
         }
-    }
+    };
 
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    // Create the video stream
+    const stream = await navigator.mediaDevices
+        .getUserMedia(constraints);
 
+    // Previews the video source
     videoElement.srcObject = stream;
     videoElement.play();
+
+    // Creates the Media Recorder
+    const options = { mimeType: 'video/webm; codecs=vp9' };
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    // Handle events
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+
 }
 
+// Captures all the recorded parts
+function handleDataAvailable(e) {
+    console.log('video data available');
+    recordedBits.push(e.data);
+}
+
+// Saves the file when the STOP BUTTON is pressed
+async function handleStop(e) {
+    const blob = new Blob(recordedBits, {
+        type: 'video/webm; codecs=vp9'
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath } = await dialog.showSaveDialog({
+        buttonLabel: 'Save Video',
+        defaultPath: `vid-${Date.now()}.webm`
+    });
+
+    if (filePath) {
+        writeFile(filePath, buffer, () => console.log('Your video was successfully created'));
+    }
+
+}
